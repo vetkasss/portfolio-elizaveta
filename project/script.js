1bdf38d22c0239c2e2a8a4f791aae43bbb605d80
vetkasss <elizavetaliask@mail.ru>
2026-04-28 16:28:51 +0300

    js change

--- project/script.js
+++ project/script.js
@@ -2,57 +2,53 @@ const sidebar = document.getElementById("sidebar");
 document.getElementById("menuBtn").onclick =
   () => sidebar.classList.toggle("open");
 
-
 const title = document.querySelector(".title");
 let ticking = false;
 
-document.addEventListener("mousemove",(e)=>{
-  if(ticking) return;
+document.addEventListener("mousemove", (e) => {
+  if (ticking) return;
   ticking = true;
 
-  requestAnimationFrame(()=>{
-    const x = (e.clientX/window.innerWidth - .5) * 12;
-    const y = (e.clientY/window.innerHeight - .5) * 12;
+  requestAnimationFrame(() => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 12;
+    const y = (e.clientY / window.innerHeight - 0.5) * 12;
     title.style.transform = `translate(${x}px,${y}px)`;
     ticking = false;
   });
 });
 
-
 const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modalImg");
 
-function openModal(src){
+function openModal(src) {
   modal.classList.add("show");
 
   const img = new Image();
   img.src = src;
 
-  img.decode().then(()=>{
+  img.decode().then(() => {
     modalImg.src = src;
   });
 }
 
-document.getElementById("close").onclick =
-  ()=> modal.classList.remove("show");
+document.getElementById("close").onclick = () =>
+  modal.classList.remove("show");
 
-modal.onclick = e=>{
-  if(e.target === modal) modal.classList.remove("show");
+modal.onclick = (e) => {
+  if (e.target === modal) modal.classList.remove("show");
 };
 
 // GALLERY
 const gallery = document.getElementById("gallery");
 
-// список сертификатов
-const certs = Array.from({length:29},(_,i)=>({
-  preview:`../certificates/previews/cert${i+1}.webp`,
-  full:`../certificates/cert${i+1}.webp`
+const certs = Array.from({ length: 29 }, (_, i) => ({
+  preview: `certificates/previews/cert${i + 1}.webp`,
+  full: `certificates/cert${i + 1}.webp`
 }));
 
-// для ленивой загрузки
-const observer = new IntersectionObserver(entries=>{
-  entries.forEach(entry=>{
-    if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
       const img = entry.target;
       img.src = img.dataset.src;
       observer.unobserve(img);
@@ -60,10 +56,9 @@ const observer = new IntersectionObserver(entries=>{
   });
 });
 
-
 const fragment = document.createDocumentFragment();
 
-certs.forEach(c=>{
+certs.forEach((c) => {
   const div = document.createElement("div");
   div.className = "item";
 
@@ -73,7 +68,7 @@ certs.forEach(c=>{
 
   observer.observe(img);
 
-  div.onclick = ()=>openModal(c.full);
+  div.onclick = () => openModal(c.full);
 
   div.appendChild(img);
   fragment.appendChild(div);
